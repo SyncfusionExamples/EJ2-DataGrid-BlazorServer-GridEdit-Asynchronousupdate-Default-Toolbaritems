91fd73045dbf8493e959e8ef6841d852076d0e0a
Pugazhenthi-Murugan <pugazhenthi.murugan@syncfusion.com>
2026-06-16 18:10:53 +0530

    update sample

---
+++ DataGridEdit/Program.cs
@@ -0,0 +1,36 @@
+using DataGridEdit.Components;
+using DataGridEdit.Data;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddSingleton<OrderService>();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+
+
+app.UseRouting();
+app.UseAntiforgery();
+app.MapControllers();
+
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
